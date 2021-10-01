4b54b7bb0fb4a75eb0fad9744f42fc63aaa9466d
shahl <shahl@192.168.1.239>
2021-09-30 21:13:49 -0400

    new code added

---
+++ src/two/GitDemo.java
@@ -0,0 +1,18 @@
+package two;
+
+public class GitDemo {
+	
+	public static void main (String[]args) {
+		System.out.println("Git demo...");
+		
+		boolean isSunday = true;
+		
+		if(isSunday) {
+			System.out.println("abc");
+			
+		}else {
+			System.out.println("xyz");
+		}
+	}
+
+}
